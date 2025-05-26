cf154ad358aee52d6a6ab54440bbbf5eea89ce1a
Juan Romero <jdrjuan@gmail.com>
2025-05-26 10:50:13 -0300

    Agrega archivo /js/main.js con funciones matemáticas
    Funciones agregadas:
    - sumar
    - restar
    - multiplicar
    - dividir
    - potencia
    - cuadrado
    - cubo

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const sumar = (a, b) => a + b;
+const restar = (a, b) => a - b;
+const multiplicar = (a, b) => a * b;
+const dividir = (a, b) => a / b;
+const potencia = (a, b) => a ** b;
+const cuadrado = a => a ** 2;
+const cubo = a => a ** 3;
