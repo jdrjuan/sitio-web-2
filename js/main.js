2448950494fb7f22150cb14f85e8a1309f8c9776
Juan Romero <jdrjuan@gmail.com>
2025-05-28 11:19:41 -0300

    Formatea contenido de js/main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 const sumar = (a, b) => a + b;
+
 const restar = (a, b) => a - b;
+
 const multiplicar = (a, b) => a * b;
+
 const dividir = (a, b) => a / b;
+
 const potencia = (a, b) => a ** b;
+
 const cuadrado = a => a ** 2;
+
 const cubo = a => a ** 3;
+
 const raizCuadrada = a => Math.sqrt(a);
+
 const raizCubica = a => Math.cbrt(a);
